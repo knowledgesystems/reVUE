835976b2b5b6c4d14148d89c6b4fd25359d2feb5
Xiang Li <xiangli5699@gmail.com>
2024-02-01 17:30:40 -0500

    Add VUEs count on home page (#12)
    * Add VUE number count on home page
    
    * Show counts in one line
    
    * Remove therapeutic implications count

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,10 +8,21 @@ interface IHomeProps {
     store: DataStore;
 }
 
-class Home extends React.Component<IHomeProps>
-{
-    public render()
-    {
+const Home: React.FC<IHomeProps> = ( props ) => {
+    const [totalGenes, setTotalGenes] = React.useState(0);
+    const [curatedVUEs, setCuratedVUEs] = React.useState(0);
+
+    React.useEffect(() => {
+        props.store.data.then(vueArray => {
+            setTotalGenes(vueArray.length);
+            let totalRevisedProteinEffects = 0;
+            vueArray.forEach(vue => {
+                totalRevisedProteinEffects += vue.revisedProteinEffects.length;
+            });
+            setCuratedVUEs(totalRevisedProteinEffects);
+        });
+    }, [props.store]);
+
         return (
             <React.Fragment>
                 <div className="jumbotron jumbotron-fluid">
@@ -19,16 +30,17 @@ class Home extends React.Component<IHomeProps>
                         <img alt='reVUE logo' src={vueLogo} width={60} style={{paddingRight:10}}/>
                         reVUE
                     </h1>
-                    <p className="lead">A Repository for Variants with Unexpected Effects (VUE) in Cancer</p>
-                    <hr className="my-4" />
+                    <p className="lead">A Repository for Variants with Unexpected Effects (VUE) in Cancer</p> 
                     <span>A curated database of known protein effects for those variants that aren't as easily predicted by conventional annotation tools.</span>
+                    <hr className="my-4" />
+                    <span>Total genes:{` `}{totalGenes}</span>
+                    <span style={{marginLeft: 50}}>Curated VUEs:{` `}{curatedVUEs}</span>
                 </div>
                 <p className='text-left'>
-                    <VUETable store={this.props.store}/>
+                    <VUETable store={props.store}/>
                 </p>
             </React.Fragment>
         );
-    }
 }
 
 export default Home;
